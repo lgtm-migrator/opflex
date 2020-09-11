85e302c279c7f8e5b804c28cffcfde8da45342b6
bashokba <bashokba@cisco.com>
2020-09-11 16:05:21 -0700

    XRevert "adding new set of test cases for secgroups related to deny rule changes"
    This reverts commit f895efbaa4836585f49d06b493d7942f27d0d262.

--- agent-ovs/lib/PolicyManager.cpp
+++ agent-ovs/lib/PolicyManager.cpp
@@ -1067,7 +1067,6 @@ static bool updatePolicyRules(OFFramework& framework,
     using modelgbp::gbp::AllowDenyAction;
     using modelgbp::gbp::RedirectAction;
     using modelgbp::gbp::RedirectDestGroup;
-    using modelgbp::gbp::LogAction;
 
     optional<shared_ptr<Parent> > parent =
         Parent::resolve(framework, parentURI);
@@ -1158,14 +1157,12 @@ static bool updatePolicyRules(OFFramework& framework,
                     RedirectDestGroup::resolve(framework, destGrpUri.get());
                     newRedirGrps.insert(destGrpUri.get());
                 }
-		else if(r->getTargetClass().get() == LogAction::CLASS_ID) {
-                      optional<shared_ptr<modelgbp::gbp::LogAction> > resloveLog = 
-                      modelgbp::gbp::LogAction::resolve(framework,r->getTargetURI().get());
-                      if (resloveLog) {
-                          ruleLog = resloveLog.get()->getLog(0) != 0 ;
-                      }
+                optional<shared_ptr<modelgbp::gbp::LogAction> > resloveLog = 
+                    modelgbp::gbp::LogAction::resolve(framework,r->getTargetURI().get());
+                if (resloveLog) {
+                   ruleLog = resloveLog.get()->getLog(0) != 0 ;
                 }
-	    }
+            }
 
             uint16_t clsPrio = 0;
             for (const shared_ptr<L24Classifier>& c : classifiers) {
